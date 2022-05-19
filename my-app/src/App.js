c367d0e1894d2af8b9f87df5938e4ac4f5c8b4ad
Narek <narek423@gmail.com>
2022-05-19 11:23:39 +0400

    Adding Mui

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import Stack from "@mui/material/Stack";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Stack direction='row' spacing={2}>
+				<Button variant='outlined' startIcon={<DeleteIcon />}>
+					Delete
+				</Button>
+				<Button variant='contained' endIcon={<SendIcon />}>
+					Send
+				</Button>
+			</Stack>
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className='App-link'
+						href='https://reactjs.org'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		</>
+	);
 }
 
 export default App;
