d3a285ff0c0a455321d6f0c6a5fa841eb0fa6494
Narek <narek423@gmail.com>
2022-05-19 11:24:56 +0400

    All aded

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Button from "@mui/material/Button";
-import DeleteIcon from "@mui/icons-material/Delete";
-import SendIcon from "@mui/icons-material/Send";
-import Stack from "@mui/material/Stack";
 
 function App() {
 	return (
-		<>
-			<Stack direction='row' spacing={2}>
-				<Button variant='outlined' startIcon={<DeleteIcon />}>
-					Delete
-				</Button>
-				<Button variant='contained' endIcon={<SendIcon />}>
-					Send
-				</Button>
-			</Stack>
-			<div className='App'>
-				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className='App-link'
-						href='https://reactjs.org'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						Learn React
-					</a>
-				</header>
-			</div>
-		</>
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
 	);
 }
 
