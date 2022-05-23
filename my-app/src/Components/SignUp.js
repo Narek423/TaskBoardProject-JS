54108749127d7922215eaa527b477144977476ed
Learlo <100930484+Learlo@users.noreply.github.com>
2022-05-23 17:30:06 +0400

    userName changed to name and added lastName

--- my-app/src/Components/SignUp.js
+++ my-app/src/Components/SignUp.js
@@ -15,252 +15,262 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 
 const useStyles = createUseStyles({
-	header: {
-		backgroundColor: "#f50157",
-		fontFamily: "Palatino",
-		fontSize: 50,
-		padding: 25,
-		margin: {
-			top: 0,
-			left: 0,
-			right: 0,
-			bottom: 5,
-		},
-	},
-	signUp: {
-		display: "flex",
-		justifyContent: "center",
-		margin: {
-			top: 0,
-			left: 0,
-			right: 0,
-			bottom: 10,
-		},
-	},
-	fields: {
-		width: 300,
-	},
-	buttons: {
-		margin: 5,
-	},
-	homeButtons: {
-		position: "fixed",
-		right: 0,
-	},
-	useSpace: {
-		marginTop: 70,
-	},
-	signInButton: {
-		appearance: "none",
-		backgroundColor: "#FFFFFF",
-		borderRadius: "40em",
-		borderStyle: "none",
-		boxShadow: "#ADCFFF 0 -12px 6px inset",
-		boxSizing: "border-box",
-		color: "#000000",
-		cursor: "pointer",
-		display: "inline-block",
-		fontFamily: "-apple-system,sans-serif",
-		fontSize: "1.2rem",
-		fontWeight: "700",
-		letterSpacing: "-.24px",
-		margin: "0",
-		outline: "none",
-		padding: "1rem 1.3rem",
-		quotes: "auto",
-		textAlign: "center",
-		textDecoration: "none",
-		transition: "all .15s",
-		userSelect: "none",
-		webkitUserSelect: "none",
-		touchAction: "manipulation",
-		"&:hover": {
-			backgroundColor: "#FFC229",
-			boxShadow: "#FF6314 0 -6px 8px inset",
-			transform: "scale(1.125)",
-		},
-		"&:active": {
-			transform: "scale(1.025)",
-		},
-	},
+  header: {
+    backgroundColor: "#f50157",
+    fontFamily: "Palatino",
+    fontSize: 50,
+    padding: 25,
+    margin: {
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 5,
+    },
+  },
+  signUp: {
+    display: "flex",
+    justifyContent: "center",
+    margin: {
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 10,
+    },
+  },
+  fields: {
+    width: 300,
+  },
+  buttons: {
+    margin: 5,
+  },
+  homeButtons: {
+    position: "fixed",
+    right: 0,
+  },
+  useSpace: {
+    marginTop: 70,
+  },
+  signInButton: {
+    appearance: "none",
+    backgroundColor: "#FFFFFF",
+    borderRadius: "40em",
+    borderStyle: "none",
+    boxShadow: "#ADCFFF 0 -12px 6px inset",
+    boxSizing: "border-box",
+    color: "#000000",
+    cursor: "pointer",
+    display: "inline-block",
+    fontFamily: "-apple-system,sans-serif",
+    fontSize: "1.2rem",
+    fontWeight: "700",
+    letterSpacing: "-.24px",
+    margin: "0",
+    outline: "none",
+    padding: "1rem 1.3rem",
+    quotes: "auto",
+    textAlign: "center",
+    textDecoration: "none",
+    transition: "all .15s",
+    userSelect: "none",
+    webkitUserSelect: "none",
+    touchAction: "manipulation",
+    "&:hover": {
+      backgroundColor: "#FFC229",
+      boxShadow: "#FF6314 0 -6px 8px inset",
+      transform: "scale(1.125)",
+    },
+    "&:active": {
+      transform: "scale(1.025)",
+    },
+  },
 });
 
 function SignUp(props) {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [userName, setUserName] = useState("");
-	const [phoneNumber, setPhoneNumber] = useState("");
-	const [taxCode, setTaxCode] = useState("");
-	const [roll, setRoll] = useState("Client");
-	const [enabled, setEnabled] = useState(false);
-	const [signInButtonHover, setSigInButtonHover] = useState(false);
-	const [signInButtonActive, setSignInButtonActive] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [taxCode, setTaxCode] = useState("");
+  const [roll, setRoll] = useState("Client");
+  const [enabled, setEnabled] = useState(false);
+  const [signInButtonHover, setSigInButtonHover] = useState(false);
+  const [signInButtonActive, setSignInButtonActive] = useState(false);
 
-	const classes = useStyles();
-	const [values, setValues] = useState({
-		amount: "",
-		password: "",
-		weight: "",
-		weightRange: "",
-		showPassword: false,
-	});
+  const classes = useStyles();
+  const [values, setValues] = useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false,
+  });
 
-	const handleChange = (prop) => (event) => {
-		setValues({ ...values, [prop]: event.target.value });
-	};
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
-	const handleClickShowPassword = () => {
-		setValues({
-			...values,
-			showPassword: !values.showPassword,
-		});
-	};
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
 
-	const handleMouseDownPassword = (event) => {
-		event.preventDefault();
-	};
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
-	// async function userSignUp() {
-	// 	try {
-	// await signup(
-	// 	email,
-	// 	password,
-	// 	userName,
-	// 	phoneNumber,
-	// 	taxCode,
-	// 	roll,
-	// 	enabled
-	// );
-	// setEmail("");
-	// setPassword("");
-	// setUserName("");
-	// setPhoneNumber("");
-	// setTaxCode("");
-	// setRoll("");
-	// setEnabled("");
-	// 	} catch (e) {
-	// 		console.log("Error");
-	// 	}
-	// }
+  // async function userSignUp() {
+  // 	try {
+  // await signup(
+  // 	email,
+  // 	password,
+  // 	userName,
+  // 	phoneNumber,
+  // 	taxCode,
+  // 	roll,
+  // 	enabled
+  // );
+  // setEmail("");
+  // setPassword("");
+  // setUserName("");
+  // setPhoneNumber("");
+  // setTaxCode("");
+  // setRoll("");
+  // setEnabled("");
+  // 	} catch (e) {
+  // 		console.log("Error");
+  // 	}
+  // }
 
-	return (
-		<>
-			<div className={classes.useSpace}>
-				<h1 className={classes.signUp}>Sign up</h1>
-				<div className={classes.signUp}>
-					<TextField
-						className={classes.fields}
-						onChange={(e) => setEmail(e.target.value)}
-						type={"email"}
-						id='emailId'
-						label='eMail (Login)'
-						variant='outlined'
-					/>
-				</div>
-				<div className={classes.signUp}>
-					<FormControl
-						className={classes.fields}
-						sx={{ m: 1, width: "25ch" }}
-						variant='outlined'
-					>
-						<InputLabel htmlFor='outlined-adornment-password'>
-							Password
-						</InputLabel>
-						<OutlinedInput
-							id='passwordId'
-							type={values.showPassword ? "text" : "password"}
-							value={values.password}
-							onChange={handleChange("password")}
-							endAdornment={
-								<InputAdornment position='end'>
-									<IconButton
-										aria-label='toggle password visibility'
-										onClick={handleClickShowPassword}
-										onMouseDown={handleMouseDownPassword}
-										edge='end'
-									>
-										{values.showPassword ? <VisibilityOff /> : <Visibility />}
-									</IconButton>
-								</InputAdornment>
-							}
-							label='Password'
-						/>
-					</FormControl>
-				</div>
-				<div className={classes.signUp}>
-					<TextField
-						className={classes.fields}
-						onChange={(e) => setUserName(e.target.value)}
-						id='userNameId'
-						label='User name'
-						variant='outlined'
-					/>
-				</div>
-				<div className={classes.signUp}>
-					<TextField
-						className={classes.fields}
-						onChange={(e) => setPhoneNumber(e.target.value)}
-						id='phoneNumberId'
-						label='Phone number'
-						variant='outlined'
-					/>
-				</div>
-				<div className={classes.signUp}>
-					<TextField
-						className={classes.fields}
-						onChange={(e) => setTaxCode(e.target.value)}
-						id='taxCodeId'
-						label='Tax code'
-						variant='outlined'
-					/>
-				</div>
-				<div className={classes.signUp}>
-					<FormControl className={classes.fields} sx={{ m: 1, width: "25ch" }}>
-						<InputLabel id='inputRollId'>Roll</InputLabel>
-						<Select
-							labelId='inputRollId'
-							id='RollId'
-							value={roll}
-							label='Age'
-							onChange={(e) => {
-								setRoll(e.target.value);
-								setEnabled(e.target.value === "Admin" ? false : true);
-							}}
-						>
-							<MenuItem value={"Client"}>Client</MenuItem>
-							<MenuItem value={"Admin"}>Admin</MenuItem>
-						</Select>
-					</FormControl>
-				</div>
-				<div className={classes.signUp} id='buttons'>
-					<button
-						className={
-							signInButtonHover
-								? classes.signInButtOnHover
-								: signInButtonActive
-								? classes.signInButtonActive
-								: classes.signInButton
-						}
-						// onClick={userSignUp}
-						onHover={() => {
-							setSigInButtonHover(true);
-							setSignInButtonActive(false);
-						}}
-						onActivate={() => {
-							setSigInButtonHover(false);
-							setSignInButtonActive(true);
-						}}
-						id='buttonSignUp'
-						variant='contained'
-						color='secondary'
-					>
-						Sign up
-					</button>
-				</div>
-			</div>
-			<Outlet />
-		</>
-	);
+  return (
+    <>
+      <div className={classes.useSpace}>
+        <h1 className={classes.signUp}>Sign up</h1>
+        <div className={classes.signUp}>
+          <TextField
+            className={classes.fields}
+            onChange={(e) => setEmail(e.target.value)}
+            type={"email"}
+            id="emailId"
+            label="eMail (Login)"
+            variant="outlined"
+          />
+        </div>
+        <div className={classes.signUp}>
+          <FormControl
+            className={classes.fields}
+            sx={{ m: 1, width: "25ch" }}
+            variant="outlined"
+          >
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="passwordId"
+              type={values.showPassword ? "text" : "password"}
+              value={values.password}
+              onChange={handleChange("password")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+        </div>
+        <div className={classes.signUp}>
+          <TextField
+            className={classes.fields}
+            onChange={(e) => setName(e.target.value)}
+            id="nameId"
+            label="Name"
+            variant="outlined"
+          />
+        </div>
+        <div className={classes.signUp}>
+          <TextField
+            className={classes.fields}
+            onChange={(e) => setLastName(e.target.value)}
+            id="lastNameId"
+            label="UsLaster name"
+            variant="outlined"
+          />
+        </div>
+        <div className={classes.signUp}>
+          <TextField
+            className={classes.fields}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            id="phoneNumberId"
+            label="Phone number"
+            variant="outlined"
+          />
+        </div>
+        <div className={classes.signUp}>
+          <TextField
+            className={classes.fields}
+            onChange={(e) => setTaxCode(e.target.value)}
+            id="taxCodeId"
+            label="Tax code"
+            variant="outlined"
+          />
+        </div>
+        <div className={classes.signUp}>
+          <FormControl className={classes.fields} sx={{ m: 1, width: "25ch" }}>
+            <InputLabel id="inputRollId">Roll</InputLabel>
+            <Select
+              labelId="inputRollId"
+              id="RollId"
+              value={roll}
+              label="Age"
+              onChange={(e) => {
+                setRoll(e.target.value);
+                setEnabled(e.target.value === "Admin" ? false : true);
+              }}
+            >
+              <MenuItem value={"Client"}>Client</MenuItem>
+              <MenuItem value={"Admin"}>Admin</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div className={classes.signUp} id="buttons">
+          <button
+            className={
+              signInButtonHover
+                ? classes.signInButtOnHover
+                : signInButtonActive
+                ? classes.signInButtonActive
+                : classes.signInButton
+            }
+            // onClick={userSignUp}
+            onHover={() => {
+              setSigInButtonHover(true);
+              setSignInButtonActive(false);
+            }}
+            onActivate={() => {
+              setSigInButtonHover(false);
+              setSignInButtonActive(true);
+            }}
+            id="buttonSignUp"
+            variant="contained"
+            color="secondary"
+          >
+            Sign up
+          </button>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
 }
 
 export default SignUp;
