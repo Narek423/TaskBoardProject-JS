215288c23b0be6051f0025cd1c44ad1469b60419
Learlo <100930484+Learlo@users.noreply.github.com>
2022-05-23 22:38:32 +0400

    added some yarn packeges connected with grid and added new component to main page for tasting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import SignUp from "./Components/SignUp";
 import SignIn from "./Components/SignIn";
 import NavMainBar from "./Components/Nav-Bar/NavMainBar";
+import PendingToEvaluation from "./Components/PendingToEvaluation";
 
 function App() {
-	return (
-		<>
-			<NavMainBar></NavMainBar>
-			<SignUp></SignUp>;
-		</>
-	);
+  return (
+    <>
+      <NavMainBar></NavMainBar>
+      <SignUp></SignUp>
+      <PendingToEvaluation></PendingToEvaluation>
+    </>
+  );
 }
 
 export default App;
