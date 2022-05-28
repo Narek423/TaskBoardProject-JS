ade6a85f293b647e784713de371b3c620f745fd7
Grigor <dav.manukyan.70@mail.ru>
2022-05-28 15:49:45 +0400

    firebase_changes

--- my-app/src/Components/firebase.js
+++ my-app/src/Components/firebase.js
@@ -1,12 +1,16 @@
 import { initializeApp } from "firebase/app";
 import {
-	browserSessionPersistence,
 	getAuth,
-	setPersistence,
+	createUserWithEmailAndPassword,
 	signInWithEmailAndPassword,
 } from "firebase/auth";
-import { useUserAuth } from "../context/UserAuthContext";
 
+import { getDatabase, ref, set, get, child } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 	apiKey: "AIzaSyAeMdajQ5RYUmEj6bxOcsVUYZYlFz55sKA",
 	authDomain: "taskboardproject-3dd1f.firebaseapp.com",
@@ -18,12 +22,8 @@ const firebaseConfig = {
 	measurementId: "G-7SS21PZV79",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-<<<<<<< HEAD
-export const auth = getAuth(app);
-
-export default app;
-=======
 const auth = getAuth();
 const database = getDatabase(app);
 export const storage = getStorage(app);
@@ -57,80 +57,41 @@ export function signup(
 		}
 	);
 }
->>>>>>> user_profile
 
-// import { getDatabase, ref, set, get, child } from "firebase/database";
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-// export const auth = getAuth();
-// const database = getDatabase(app);
+function writeUserData(
+	user,
+	password,
+	name,
+	lastName,
+	dateOfBirth,
+	phoneNumber,
+	taxCode,
+	roll,
+	enabled,
+	avatar = "https://st2.depositphotos.com/1104517/11965/v/950/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
+) {
+	set(ref(database, "users/" + user.uid), {
+		email: user.email,
+		avatar,
+		name,
+		lastName,
+		dateOfBirth,
+		phoneNumber,
+		taxCode,
+		roll,
+		enabled,
+	});
+}
 
-// export function signup(
-// 	email,
-// 	password,
-// 	name,
-// 	lastName,
-// 	dateOfBirth,
-// 	phoneNumber,
-// 	taxCode,
-// 	roll,
-// 	enabled
-// ) {
-// 	console.log(email, password);
-// 	return createUserWithEmailAndPassword(auth, email, password).then(
-// 		(userCredential) => {
-// 			const user = userCredential.user;
-// 			writeUserData(
-// 				user,
-// 				password,
-// 				name,
-// 				lastName,
-// 				dateOfBirth,
-// 				phoneNumber,
-// 				taxCode,
-// 				roll,
-// 				enabled
-// 			);
-// 		}
-// 	);
-// }
+export async function signin(email, password) {
+	console.log("entered");
+	return signInWithEmailAndPassword(auth, email, password).then(
+		(userCredential) => {
+			const user = userCredential.user;
+			const userId = user.uid;
+			const dbRef = ref(getDatabase());
+			return get(child(dbRef, `users/${userId}`)).then((snep) => snep.val());
+		}
+	);
+}
 
-<<<<<<< HEAD
-// function writeUserData(
-// 	user,
-// 	password,
-// 	name,
-// 	lastName,
-// 	dateOfBirth,
-// 	phoneNumber,
-// 	taxCode,
-// 	roll,
-// 	enabled,
-// 	avatar = "https://st2.depositphotos.com/1104517/11965/v/950/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
-// ) {
-// set(ref(database, "users/" + user.uid), {
-// 	email: user.email,
-// 	avatar,
-// 	name,
-// 	lastName,
-// 	dateOfBirth,
-// 	phoneNumber,
-// 	taxCode,
-// 	roll,
-// 	enabled,
-// });
-// }
-
-// export async function signin(email, password) {
-// 	console.log("entered");
-// 	return signInWithEmailAndPassword(auth, email, password).then(
-// 		(userCredential) => {
-// 			const user = userCredential.user;
-// 			const userId = user.uid;
-// 			const dbRef = ref(getDatabase());
-// 			return get(child(dbRef, `users/${userId}`)).then((snep) => snep.val());
-// 		}
-// 	);
-// }
-=======
->>>>>>> user_profile
