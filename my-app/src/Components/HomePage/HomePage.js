79fc46ea0f391e4e072db63b60a36bec3c417661
Grigor <dav.manukyan.70@mail.ru>
2022-06-15 01:59:00 +0400

    update

--- my-app/src/Components/HomePage/HomePage.js
+++ my-app/src/Components/HomePage/HomePage.js
@@ -1,204 +1,159 @@
 import React, { useRef } from "react";
 import NavMainBar from "../Nav-Bar/NavMainBar";
-import img1 from "../img/img.jpg";
+import img1 from "../img/img.jpg"
 import { createUseStyles } from "react-jss";
-import { useUserAuth } from "../../context/UserAuthContext";
-import { useNavigate } from "react-router-dom";
 
 const useStyle = createUseStyles(() => {
-	return {
-		maindiv: {
-			height: "100vh",
-			display: "flex",
-			flexDirection: "column",
-		},
-		container: {
-			flex: 10,
-			overflowY: "scroll",
-			backgroundColor: "#b3ffe6",
+    return {
+        maindiv:{
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        container: {
+            flex: 10,
+            overflowY: 'scroll',
+            backgroundColor: '#b3ffe6',
 
-			"&::-webkit-scrollbar": {
-				width: 14,
-			},
-			"&::-webkit-scrollbar-track": {
-				background: "grey",
-			},
-			"&::-webkit-scrollbar-thumb": {
-				background: "#019CAD",
-			},
-			"&::-webkit-scrollbar-thumb:hover": {
-				background: "#555",
-			},
-		},
-	};
-});
+            "&::-webkit-scrollbar": {
+             width: 14,
+           },
+           "&::-webkit-scrollbar-track": {
+             background: "grey",
+           },
+           "&::-webkit-scrollbar-thumb": {
+             background: "#019CAD",
+           },
+           "&::-webkit-scrollbar-thumb:hover": {
+             background: "#555",
+           },
+       }
+    }
+})
 function HomePage(params) {
-	const classes = useStyle();
-	const about = useRef();
-	const contactUs = useRef();
-	const navigate = useNavigate();
-	const { user } = useUserAuth();
-	if (user) navigate("/profile");
+    const classes = useStyle();
+    const about = useRef();
+    const contactUs = useRef();
 
-	const scrollTo = (ref) => {
-		return ref?.current?.scrollIntoView({
-			block: "center",
-			behavior: "smooth",
-		});
-	};
+   
+    const scrollTo = (ref) => {
+      return ref?.current?.scrollIntoView({block: 'center', behavior: "smooth"})
+    }
 
-	return user ? null : (
-		<div
-			className={classes.maindiv}
-			//  style={{
-			//     height: '100vh',
-			//     display: 'flex',
-			//     flexDirection: 'column'
-			// }}
-		>
-			<NavMainBar about={about} contactUs={contactUs} scrollTo={scrollTo} />
-			{/* <button onClick={() => {
+
+  return (
+    <div  
+      ref={about}
+      className={classes.maindiv}
+    //  style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     flexDirection: 'column'
+    // }}
+    >
+      <NavMainBar 
+      about={about} contactUs={contactUs} scrollTo={scrollTo}
+      />
+      {/* <button onClick={() => {
         return about?.current?.scrollIntoView({block: 'center', behavior: "smooth"})
       }}></button>
            <button onClick={() => {
         return contactUs?.current?.scrollIntoView({block: 'center', behavior: "smooth"})
       }}></button> */}
-			<div /////scroll@ chi haskanum???????
-				className={classes.container}
-				//    style={{
-				//        flex: 10,
-				//        overflowY: 'scroll',
-				//        "&::-webkit-scrollbar": {
-				//         width: 4,
-				//       },
-				//       "&::-webkit-scrollbar-track": {
-				//         background: "grey",
-				//       },
-				//       "&::-webkit-scrollbar-thumb": {
-				//         background: "#019CAD",
-				//       },
-				//       "&::-webkit-scrollbar-thumb:hover": {
-				//         background: "#555",
-				//       },
-				//   }}
-			>
-				<div
-					ref={contactUs}
-					style={{
-						height: 550,
-						backgroundColor: "#99ffdd",
-						marginTop: "4%",
-						alignItems: "center",
-						float: "right",
-						display: "flex",
-						textAlign: "center",
-					}}
-				>
-					<div
-						style={{
-							height: "100%",
-							height: "90%",
-							backgroundImage: `url("${img1}")`,
-							backgroundSize: "100% 100%",
-							alignItems: "center",
-							flex: 5,
-						}}
-					></div>
-					<div
-						style={{
-							flex: 4,
-						}}
-					>
-						<h1>Create own tasks</h1>
-						<span>
-							asssssssssssd naskdgasjn gas uiasjdi ajduiojas iudjasiy jiuasj
-							ilasjdioyjasodn adjsioajdt asssssssssssddasduiasj
-							dasmhdlkjaslkdiopasjdiouaskjduiasjkduasjdskjdysaokjtdjoasd
-							asdkjgmnasuilm,dioasyhkdmuiasjdoukasjndiu
-						</span>
-					</div>
-				</div>
+      <div                                      /////scroll@ chi haskanum???????
+          className={classes.container}
+    //    style={{
+    //        flex: 10,
+    //        overflowY: 'scroll',
+    //        "&::-webkit-scrollbar": {
+    //         width: 4,
+    //       },
+    //       "&::-webkit-scrollbar-track": {
+    //         background: "grey",
+    //       },
+    //       "&::-webkit-scrollbar-thumb": {
+    //         background: "#019CAD",
+    //       },
+    //       "&::-webkit-scrollbar-thumb:hover": {
+    //         background: "#555",
+    //       },
+    //   }}
+      >
+      <div 
+      ref={contactUs}
+          style={{
+          height: 550,
+          backgroundColor: '#99ffdd',
+          marginTop: '4%',
+          alignItems: 'center',
+          float: "right",         
+          display: "flex",
+          textAlign: 'center'
+      }}>
+      <div
+        style={{
+          height: "100%",
+          height: "90%",
+          backgroundImage: `url("${img1}")`,
+          backgroundSize: "100% 100%",
+          alignItems: 'center',
+          flex: 5
+        }}
+      >
+       
+      </div>
+      <div style={{
+          flex: 4,
+      }}>
+          <h1>Create own tasks</h1>
+      <span >asssssssssssd naskdgasjn gas uiasjdi ajduiojas iudjasiy jiuasj ilasjdioyjasodn adjsioajdt asssssssssssddasduiasj
+            dasmhdlkjaslkdiopasjdiouaskjduiasjkduasjdskjdysaokjtdjoasd
+            asdkjgmnasuilm,dioasyhkdmuiasjdoukasjndiu
+        </span>
+        </div>
+      </div>
+      
 
-				<div
-					style={{
-						height: 550,
-						backgroundColor: "#99ffdd",
-						marginTop: "4%",
-						alignItems: "center",
-						float: "right",
-						display: "flex",
-						textAlign: "center",
-					}}
-				>
-					<div
-						style={{
-							flex: 4,
-						}}
-					>
-						<h1>Create own tasks</h1>
-						<span>
-							asssssssssssd naskdgasjn gas uiasjdi ajduiojas iudjasiy jiuasj
-							ilasjdioyjasodn adjsioajdt asssssssssssddasduiasj
-							dasmhdlkjaslkdiopasjdiouaskjduiasjkduasjdskjdysaokjtdjoasd
-							asdkjgmnasuilm,dioasyhkdmuiasjdoukasjndiu
-						</span>
-					</div>
-					<div
-						style={{
-							height: "100%",
-							height: "90%",
-							backgroundImage: `url("${img1}")`,
-							backgroundSize: "100% 100%",
-							alignItems: "center",
-							flex: 5,
-						}}
-					></div>
-				</div>
 
-				<div
-					ref={about}
-					style={{
-						height: 550,
-						backgroundColor: "#99ffdd",
-						marginTop: "4%",
-						alignItems: "center",
-						float: "right",
-						display: "flex",
-						textAlign: "center",
-					}}
-				>
-					<div
-						style={{
-							height: "100%",
-							height: "90%",
-							backgroundImage: `url("${img1}")`,
-							backgroundSize: "100% 100%",
-							alignItems: "center",
-							flex: 5,
-						}}
-					></div>
-					<div
-						style={{
-							flex: 4,
-						}}
-					>
-						<h1>Create own tasks</h1>
-						<span>
-							asssssssssssd naskdgasjn gas uiasjdi ajduiojas iudjasiy jiuasj
-							ilasjdioyjasodn adjsioajdt asssssssssssddasduiasj
-							dasmhdlkjaslkdiopasjdiouaskjduiasjkduasjdskjdysaokjtdjoasd
-							asdkjgmnasuilm,dioasyhkdmuiasjdoukasjndiu
-						</span>
-					</div>
-				</div>
+      <div 
+          style={{
+          height: 550,
+          backgroundColor: '#99ffdd',
+          marginTop: '4%',
+          alignItems: 'center',
+          float: "right",         
+          display: "flex",
+          textAlign: 'center',
+      }}>
+           <div style={{
+          flex: 4
+      }}>
+          <h1>Create own tasks</h1>
+      <span >asssssssssssd naskdgasjn gas uiasjdi ajduiojas iudjasiy jiuasj ilasjdioyjasodn adjsioajdt asssssssssssddasduiasj
+            dasmhdlkjaslkdiopasjdiouaskjduiasjkduasjdskjdysaokjtdjoasd
+            asdkjgmnasuilm,dioasyhkdmuiasjdoukasjndiu
+        </span>
+        </div>
+      <div
+        style={{
+          height: "100%",
+          height: "90%",
+          backgroundImage: `url("${img1}")`,
+          backgroundSize: "100% 100%",
+          alignItems: 'center',
+          flex: 5
+        }}
+      >
+       
+      </div>
+     
 
-<<<<<<< HEAD
 
      
       </div> 
 
       <div 
-      ref={about}
+          ref={about}
           style={{
           height: 550,
           backgroundColor: '#99ffdd',
@@ -252,25 +207,6 @@ function HomePage(params) {
       </div>
     </div>
   );
-=======
-				<div
-					style={{
-						textAlign: "center",
-						backgroundColor: "#99ffdd",
-						marginTop: "4%",
-					}}
-				>
-					<h1>Abot us</h1>
-					<span>
-						saaaaaaaaaadylkasudjasdiasjd
-						sfdsfsdddddddddddddddddddddddddddddddddddddddddddddddddf sdfsdf
-						dsfdssssss dfsdf fds fsdfsd fsdfsdfsd sdfsdfsdfdsfsdfsdfsd
-					</span>
-				</div>
-			</div>
-		</div>
-	);
->>>>>>> origin/main
 }
 
 export default HomePage;
