645a04daa4c893cd8a846e4f8f18eb1e05e27c49
Grigor <dav.manukyan.70@mail.ru>
2022-06-15 03:17:06 +0400

    routes

--- my-app/src/constants/Paths.js
+++ my-app/src/constants/Paths.js
@@ -8,8 +8,8 @@ const paths = {
   ACCEPTION_TASKS_PATH: "acceptiontasks",
   CREATE_TASK_PATH: "createtask",
   EVALUATION_TASKS_PATH: "evaluationtasks",
-  REJECTED_TASKS_PATH: "recectedtasks",
-  IN_PROCCESS_TASKS_PATH: "inprocessasks",
+  REJECTED_TASKS_PATH: "rejectedtasks",
+  IN_PROCCESS_TASKS_PATH: "inprocesstasks",
   DONE_TASKS_PATH: "donetasks",
   STATICS_PATH: "statics",
 };
