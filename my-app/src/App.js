d49c2c8fa6d80c7786d57d837c6cef4af9365ecb
Learlo <100930484+Learlo@users.noreply.github.com>
2022-06-22 13:41:04 +0400

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,11 +4,10 @@ import SignIn from "./components/SignIn";
 import { Route, Routes } from "react-router-dom";
 import NavMainBar from "./components/Nav-Bar/HomePageNavBar";
 import { UserAuthContextProvider } from "./context/UserAuthContext";
-import Profile from "./components/UserProfile/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
 import paths from "./constants/Paths";
 import HomePage from "./components/HomePage/HomePage";
-import UserProfile from "./components/UserProfile/Profile";
+import UserProfile from "./components/UserProfile/UserProfile";
 
 function App() {
   const { USER_PROFILE_PATH, SIGN_IN_PATH, SIGN_UP_PATH } = paths;
